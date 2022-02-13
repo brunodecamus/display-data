fb62e3fb070af8737e070f583d070df683cbbc08
brunodecamus <bruno.decamus@gmail.com>
2022-02-13 11:06:01 +0100

    Plein de box dans About

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
   { path: 'products', component: ProductListComponent },
   { path: 'products/:id', component: ProductDetailComponent },
+  { path: 'about', component: AboutComponent },
   { path: '', redirectTo: '/products', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { AboutComponent } from './about/about.component';
+import { Test1Component } from './about/test1/test1.component';
+import { Test2Component } from './about/test2/test2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    AboutComponent,
+    Test1Component,
+    Test2Component
   ],
   imports: [
     BrowserModule,
