f61234b9d1b8ab6dbfdda963b974355b48e42cc3
brunodecamus <bruno.decamus@gmail.com>
2022-03-04 20:58:02 +0100

    Customise le service

--- src/app/services/random-user.service.ts
+++ src/app/services/random-user.service.ts
@@ -28,7 +28,7 @@ export class RandomUserService {
     return this.http.get('https://randomuser.me/api/?results=20');
   }
 
-  getRandomUsers() {
-    return this.http.get<any>('https://randomuser.me/api/?results=20&nat=us,dk,fr,gb');
+  getRandomUsers(num: number) {
+    return this.http.get<any>('https://randomuser.me/api/?results=' + num + '&nat=us,dk,fr,gb');
   }
 }
